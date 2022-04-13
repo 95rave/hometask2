d6305c349ef7c87a74f60e2803273bd65de530c7
95rave <ravanaap@code.edu.az>
2022-04-13 20:30:17 +0400

    sorry

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int number1, number2;
+
+            Console.Write("Enter the First number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Second number:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2)
+            
+            Console.WriteLine("{0} is the Greater number than {1}" , number1, number2);
+
+            else
+             Console.WriteLine("{0} is the Greater number than {1}", number2, number1);
+
+            Console.ReadLine();
+
+
+         }
+        
+    }
+}
